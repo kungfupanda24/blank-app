463a5564d04633f3f8357587311e0f92a88e8b84
kungfupanda24 <lyfzexplore@gmail.com>
2024-10-30 13:58:10 +0000

    FirstCommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Yahoo!My brand new Streamlit app")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("Happy Diwali!!!!!")
