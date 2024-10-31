cda4a997b4120952b9752be3099f8cf269870138
kungfupanda24 <lyfzexplore@gmail.com>
2024-10-31 12:17:12 +0000

    map

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.title("Happy Diwali!!!!!")
+st.title("Happy Diwali Sumit!!!!!")
